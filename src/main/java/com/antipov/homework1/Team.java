1da3eec1a27a37731b20aaba899d50b2e398e5a0
PARSEK\IUntype <vaisdreik@gmail.com>
2018-08-21 13:02:32 +0300

    update Team class: -add and fill list of participants -update method 'showResult' -add method 'Execute', where team members are tested.

--- src/main/java/com/antipov/homework1/Team.java
+++ src/main/java/com/antipov/homework1/Team.java
@@ -1,7 +1,32 @@
 package com.antipov.homework1;
 
-public class Team {
-    public void showResults() {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Team {
+
+    private List<Participant> teamMembers = new ArrayList<Participant>();
+
+    public Team() {
+        teamMembers.add(new Participant("Алексей"));
+        teamMembers.add(new Participant("Борис"));
+        teamMembers.add(new Participant("Галина"));
+        teamMembers.add(new Participant("Евгений"));
+    }
+
+    public void showResults() {
+        System.out.println("Результаты:");
+        for (Participant member :
+                teamMembers) {
+            System.out.println(member.getName() + ": " + (member.isPassing() ?
+                    "прошел все испытания" : "не прошел испытания"));
+        }
+    }
+
+    public void Execute(Obstacle test) {
+        for (Participant member : teamMembers) {
+            if (member.isPassing() & !test.checkPassing(member))
+                member.setPassing(false);
+        }
     }
 }
