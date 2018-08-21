3fe14bfee8671dffd537257def6787372f81fa60
PARSEK\IUntype <vaisdreik@gmail.com>
2018-08-21 13:35:56 +0300

    update team: add team name and method show all team members update main with team

--- src/main/java/com/antipov/homework1/Main.java
+++ src/main/java/com/antipov/homework1/Main.java
@@ -3,7 +3,8 @@ package com.antipov.homework1;
 public class Main {
     public static void main(String[] args) {
         Course course = new Course();
-        Team team = new Team();
+        Team team = new Team("Гики");
+        team.showTeam();
         course.doIt(team);
         team.showResults();
     }
--- src/main/java/com/antipov/homework1/Team.java
+++ src/main/java/com/antipov/homework1/Team.java
@@ -4,20 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Team {
-
+    private String name;
     private List<Participant> teamMembers = new ArrayList<Participant>();
 
-    public Team() {
+    public Team(String name) {
+        this.name = name;
         teamMembers.add(new Participant("Алексей"));
         teamMembers.add(new Participant("Борис"));
-        teamMembers.add(new Participant("Галина"));
+        teamMembers.add(new Participant("Дмитрий"));
         teamMembers.add(new Participant("Евгений"));
+    }
+
+    public void showTeam() {
+        System.out.println("Комманда \"" + name + "\"");
+        System.out.println("Имя     (характеристики:ST,DX,EN,IQ)");
+        for (Participant member : teamMembers) {
+            System.out.println(
+                    member.getName() + " (" + member.getStrength() + "," + member.getDexterity() + "," +
+                    member.getEndurance() + "," + member.getIntellect() + ")");
+        }
     }
 
     public void showResults() {
         System.out.println("Результаты:");
-        for (Participant member :
-                teamMembers) {
+        for (Participant member : teamMembers) {
             System.out.println(member.getName() + ": " + (member.isPassing() ?
                     "прошел все испытания" : "не прошел испытания"));
         }
